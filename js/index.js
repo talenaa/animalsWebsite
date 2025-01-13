7bee8f8ac1eeba8ae2c14d91449d1353700832ed
talenaa <talenabarbosa7@gmail.com>
2025-01-13 03:36:28 +0100

    adding js

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const backToTopButton = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+document.querySelectorAll('.social a img').forEach(icon => {
+    icon.addEventListener('mouseenter', () => {
+        icon.style.filter = 'brightness(1.5)';
+    });
+    icon.addEventListener('mouseleave', () => {
+        icon.style.filter = 'brightness(1)';
+    });
+});
