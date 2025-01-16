4417701a841cdb65b2feb739d05473a9ce41152d
talenaa <talenabarbosa7@gmail.com>
2025-01-16 11:40:04 +0100

    finished gray wolf gallery

--- js/wolves.js
+++ js/wolves.js
@@ -20,3 +20,28 @@ document.querySelectorAll('.social a img').forEach(icon => {
         icon.style.filter = 'brightness(1)';
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('imageModal');
+    const modalImg = document.getElementById('modalImage');
+    const captionText = document.getElementById('caption');
+    const closeModal = document.querySelector('.close');
+
+    document.querySelectorAll('.galleryItem img').forEach(img => {
+        img.addEventListener('click', () => {
+          modal.style.display = 'block';
+          modalImg.src = img.src;
+          captionText.innerText = img.alt;
+        });
+      });
+
+      closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+      });
+      
+      modal.addEventListener('click', (event) => {
+        if (event.target=== modal) {
+            modal.style.display = 'none';
+        }
+      });
+    });
