991c58e45852b35907fac0088913454070f84985
talenaa <talenabarbosa7@gmail.com>
2025-01-17 10:04:37 +0100

    finished ethiopian wolves gallery

--- js/wolves.js
+++ js/wolves.js
@@ -97,3 +97,16 @@ document.addEventListener('DOMContentLoaded', () => {
             modal.style.display = 'none';
         }
       });
+
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+          const target = document.querySelector(this.getAttribute('href'));
+          if (target) {
+            target.scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+            });
+          }
+        });
+      });
